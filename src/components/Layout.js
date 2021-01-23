e4acfe617845cd86f92758b157b3dbc96394f70d
Peter Adkins <b13ltd@Peters-MacBook-Pro.local>
2021-01-23 21:18:29 +0000

    Updated mobile nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import '../styles/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Particles from 'react-particles-js';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/pro-regular-svg-icons';
 import Header from './Header';
@@ -67,9 +69,59 @@ const particleOptions = {
 }
 
 const Layout = (props) => {
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar)
+
     return (
+        <>
+        <div className="navbar">
+                <Link to="#" className="menu-bars">
+                    <FaIcons.FaBars onClick={showSidebar} />
+                </Link>
+            </div>
+            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+                <ul className="nav-menu-items" onClick={showSidebar}>
+                    <li className="navbar-toggle">
+                        <Link to="#" className="menu-bars">
+                            <AiIcons.AiOutlineClose />
+                        </Link>
+                    </li>
+                            <li className="nav-text">
+                                <Link to="/">
+                                    <span>Home</span>
+                                </Link>
+                            </li>
+                            <li className="nav-text">
+                                <Link to="/about">
+                                    <span>About</span>
+                                </Link>
+                            </li>
+                            <li className="nav-text">
+                                <Link to="/skills">
+                                    <span>Skills</span>
+                                </Link>
+                            </li>
+                            <li className="nav-text">
+                                <Link to="/projects">
+                                    <span>Portfolio</span>
+                                </Link>
+                            </li>
+                            <li className="nav-text">
+                                <Link to="/contact">
+                                    <span>Contact</span>
+                                </Link>
+                            </li>
+                            <li className="nav-text">
+                                <Link to="/blog">
+                                    <span>Blog</span>
+                                </Link>
+                            </li>    
+                </ul>
+            </nav>
+
         <div className="containerStyle container contentStyle">
-                    <a className="menuIcon"><FontAwesomeIcon icon={faBars} /></a>
+            {/* <a className="menuIcon"><FontAwesomeIcon icon={faBars} /></a> */}
             <Link to="/"><img class="logo" src={require('../images/icon.png')} /></Link>
             <div onClick={dayNightMode} class="toggle"></div>
             <Particles
@@ -79,6 +131,7 @@ const Layout = (props) => {
                 {props.children}
             <Footer />
         </div>
+        </>
     )
 }
 
